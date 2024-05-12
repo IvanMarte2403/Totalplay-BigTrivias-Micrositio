7278eb9e9597fafeff1c4c14effbcb235b283ee6
IvanMarte <ivanbiasfx@gmail.com>
2024-05-11 20:00:12 -0600

    New Pull Preguntas

--- juego-1/js/preguntas.js
+++ juego-1/js/preguntas.js
@@ -1,28 +1,62 @@
 let preguntas = {
     'historia': [
-        { pregunta: '¿Cuántas finales de la UEFA Champions League han sido en el estadio de Wembley?', respuestas: ['8', '3', '5'], respuestaCorrecta: 1 },
-        { pregunta: '¿Quién ganó la final de la UEFA Champions League en Wembley del 2011?', respuestas: ['Real Madrid', 'FC Barcelona', 'Manchester United'], respuestaCorrecta: 1 },
-        { pregunta: '¿Cuántos títulos tiene el equipo más ganador en la historia de la Champions?', respuestas: ['14', '16', '8'], respuestaCorrecta: 0 }
+        { pregunta: '¿En qué país se originó el fútbol moderno?', respuestas: ['Italia', 'Inglaterra', 'España'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál fue el primer país no europeo en ganar una Copa del Mundo FIFA?', respuestas: ['Brasil', 'Argentina', 'Uruguay'], respuestaCorrecta: 2 },
+        { pregunta: '¿Qué equipo ganó la primera Copa del Mundo FIFA en 1930?', respuestas: ['Brasil', 'Uruguay', 'Alemania'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál es el club más antiguo conocido que aún juega fútbol?', respuestas: ['FC Barcelona', 'Sheffield FC', 'AC Milan'], respuestaCorrecta: 1 },
+        { pregunta: '¿En qué año se fundó la FIFA?', respuestas: ['1904', '1910', '1923'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué país ha ganado más Copas del Mundo FIFA?', respuestas: ['Brasil', 'Alemania', 'Italia'], respuestaCorrecta: 0 },
+        { pregunta: '¿Cuál fue el primer país asiático en organizar una Copa del Mundo FIFA?', respuestas: ['Japón', 'Corea del Sur', 'Ambos'], respuestaCorrecta: 2 },
+        { pregunta: '¿Quién es conocido como el "rey del fútbol"?', respuestas: ['Diego Maradona', 'Pelé', 'Lionel Messi'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál fue el primer torneo internacional de fútbol?', respuestas: ['Juegos Olímpicos', 'Copa América', 'Eurocopa'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué año marcó la introducción del sistema de tarjetas amarillas y rojas en el fútbol?', respuestas: ['1966', '1970', '1974'], respuestaCorrecta: 1 }
     ],
     'geografia': [
-        { pregunta: '¿Cuál es la capacidad del el Estadio de Wembley?', respuestas: ['75,000', '80,000', '90,000'], respuestaCorrecta: 2 },
-        { pregunta: '¿En qué año fue reinaugurado emblemático Wembley?', respuestas: ['2007', '2003', '2018'], respuestaCorrecta: 0 },
-        { pregunta: '¿Qué equipos participaron en la final del 2013?', respuestas: ['Juventus y Real Madrid', 'Bayern Munich y Borussia Dortmund', 'Liverpool F.C. y AC Milan'], respuestaCorrecta: 1 }
+        { pregunta: '¿En qué ciudad se encuentra el estadio Camp Nou?', respuestas: ['Madrid', 'Barcelona', 'Sevilla'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué país es conocido por tener una liga de fútbol llamada "La Liga"?', respuestas: ['Italia', 'España', 'Francia'], respuestaCorrecta: 1 },
+        { pregunta: '¿Dónde se celebró la Copa del Mundo FIFA 2014?', respuestas: ['Brasil', 'Alemania', 'Argentina'], respuestaCorrecta: 0 },
+        { pregunta: '¿Cuál es el país de origen del club de fútbol Boca Juniors?', respuestas: ['Brasil', 'Argentina', 'Chile'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué país ha albergado la Copa del Mundo FIFA en tres ocasiones?', respuestas: ['México', 'Italia', 'Francia'], respuestaCorrecta: 0 },
+        { pregunta: '¿En qué continente se juega la CAF Champions League?', respuestas: ['Asia', 'África', 'América del Sur'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál es el país con más clubes en la UEFA Champions League?', respuestas: ['España', 'Inglaterra', 'Alemania'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué país sudamericano es famoso por su rivalidad futbolística entre River Plate y Boca Juniors?', respuestas: ['Brasil', 'Uruguay', 'Argentina'], respuestaCorrecta: 2 },
+        { pregunta: '¿Cuál es el país más pequeño en calificar para una Copa del Mundo FIFA?', respuestas: ['Trinidad y Tobago', 'Islandia', 'Jamaica'], respuestaCorrecta: 1 },
+        { pregunta: '¿Dónde se celebró la primera Copa del Mundo FIFA de la historia?', respuestas: ['Uruguay', 'Argentina', 'Brasil'], respuestaCorrecta: 0 }
     ],
     'ciencia': [
-        { pregunta: '¿Cuántos equipos participarán en la UEFA Champions League 24/25?', respuestas: ['32', '36', '30'], respuestaCorrecta: 1 },
-        { pregunta: '¿Cuántos son los partidos del jugador del Real Madrid con más partidos en la UEFA Champions League?', respuestas: ['132', '131', '152'], respuestaCorrecta: 2 },
-        { pregunta: '¿Cuántas finales de Champions League ha perdido Carlo Ancelotti?', respuestas: ['2', '0', '1'], respuestaCorrecta: 2 }
+        { pregunta: '¿Cuál es el récord mundial para la mayor asistencia en un partido de fútbol?', respuestas: ['199,854', '150,000', '120,000'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué tecnología se utiliza para determinar si el balón ha cruzado la línea de gol?', respuestas: ['VAR', 'Hawk-Eye', 'GoalRef'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuánto dura el promedio de un sprint en un partido de fútbol profesional?', respuestas: ['2 segundos', '4 segundos', '6 segundos'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué es la "altura" en el contexto del fútbol?', respuestas: ['La elevación del estadio', 'La táctica de juego', 'El salto de un jugador'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué gas se utiliza comúnmente para inflar los balones de fútbol?', respuestas: ['Oxígeno', 'Nitrógeno', 'Helio'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cómo afecta la altitud a la distancia que puede viajar un balón de fútbol?', respuestas: ['Viaja menos distancia', 'No afecta', 'Viaja más distancia'], respuestaCorrecta: 2 },
+        { pregunta: '¿Cuál es la frecuencia cardíaca promedio de un futbolista durante un partido?', respuestas: ['140 bpm', '160 bpm', '180 bpm'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué músculos son los más exigidos en el fútbol?', respuestas: ['Isquiotibiales', 'Cuádriceps', 'Ambos'], respuestaCorrecta: 2 },
+        { pregunta: '¿Qué efecto tiene el entrenamiento en altitud en los jugadores de fútbol?', respuestas: ['Mejora la resistencia', 'Reduce la capacidad de sprint', 'No tiene efecto'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué técnica de imagen se utiliza para diagnosticar lesiones en jugadores de fútbol?', respuestas: ['Radiografía', 'Resonancia magnética', 'Ultrasonido'], respuestaCorrecta: 1 }
     ],
     'arte': [
-        { pregunta: '¿Cuáles son los idiomas oficiales de la UEFA Champions League?', respuestas: ['Alemán, inglés e italiano', 'Inglés, alemán y francés', 'Francés, español y alemán'], respuestaCorrecta: 1 },
-        { pregunta: '¿En qué idioma esta escrito el himno de la UEFA Champions League?', respuestas: ['Alemán, inglés e italiano', 'Francés, español y alemán', 'Inglés, alemán y francés'], respuestaCorrecta: 2 },
-        { pregunta: '¿A quién se le otorga de manera permanente el trofeo De la Copa de Europa en la final de la Champions League?', respuestas: ['Al ganador del torneo en curso o 2 veces consecutivas', 'Al ganador consecutivo de 3 torneos o 2 veces consecutivas', 'Al ganador consecutivo de 3 torneos o 5 veces alternas'], respuestaCorrecta: 2 }
+        { pregunta: '¿Quién diseñó el trofeo actual de la Copa del Mundo?', respuestas: ['Silvio Gazzaniga', 'Jules Rimet', 'Adidas'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué famoso artista realizó una obra inspirada en el fútbol durante la Copa del Mundo 1982?', respuestas: ['Pablo Picasso', 'Andy Warhol', 'Salvador Dalí'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué película sobre fútbol fue dirigida por Danny Boyle?', respuestas: ['Escape to Victory', 'The Damned United', 'Bend it Like Beckham'], respuestaCorrecta: 2 },
+        { pregunta: '¿Cuál es el tema principal de la canción "Waka Waka" de Shakira para el Mundial 2010?', respuestas: ['La unidad', 'La celebración del fútbol', 'La lucha contra la adversidad'], respuestaCorrecta: 1 },
+        { pregunta: '¿Qué pintor español creó una serie de obras inspiradas en el fútbol?', respuestas: ['Joan Miró', 'Pablo Picasso', 'Salvador Dalí'], respuestaCorrecta: 2 },
+        { pregunta: '¿Qué famosa banda británica grabó una canción para la Eurocopa 1996?', respuestas: ['The Beatles', 'Queen', 'The Rolling Stones'], respuestaCorrecta: 1 },
+        { pregunta: '¿En qué película aparece un personaje principal que es un fanático del fútbol?', respuestas: ['Fever Pitch', 'Goal!', 'The Football Factory'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué obra teatral se centra en la vida de un famoso futbolista?', respuestas: ['The Beautiful Game', 'Bend it Like Beckham', 'In the Hands of the Gods'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál es la obra más famosa de un pintor que retrata el fútbol?', respuestas: ['Pelé', 'The Football Match', 'Football Players'], respuestaCorrecta: 2 },
+        { pregunta: '¿Qué documental explora la historia detrás de un famoso club de fútbol?', respuestas: ['Take the Ball, Pass the Ball', 'An Impossible Job', 'Once in a Lifetime'], respuestaCorrecta: 0 }
     ],
     'deporte': [
-        { pregunta: '¿Qué equipo ha jugado más de 300 partidos en la historia de La Champions League?', respuestas: ['Real Madrid', 'Liverpool', 'FC Barcelona'], respuestaCorrecta: 0 },
-        { pregunta: '¿Cuál de los siguientes equipos tiene derecho a portar el emblema de campeón múltiple en su uniforme?', respuestas: ['Brujas', 'Bayern', 'Tottenham'], respuestaCorrecta: 1 },
-        { pregunta: '¿Quién es el jugador con más tarjetas rojas en la Champions?', respuestas: ['Zlatan Ibrahimović', 'Sergio Ramos', 'Cristiano Ronaldo'], respuestaCorrecta: 1 }
+        { pregunta: '¿Qué equipo tiene el récord de más títulos consecutivos en la Premier League?', respuestas: ['Manchester United', 'Arsenal', 'Chelsea'], respuestaCorrecta: 0 },
+        { pregunta: '¿Cuál es la máxima cantidad de goles marcados por un jugador en un solo partido de la Copa del Mundo?', respuestas: ['5', '3', '7'], respuestaCorrecta: 0 },
+        { pregunta: '¿Quién es el máximo goleador en la historia de la Champions League?', respuestas: ['Cristiano Ronaldo', 'Lionel Messi', 'Raúl'], respuestaCorrecta: 0 },
+        { pregunta: '¿Cuál es la competición de clubes más antigua del mundo?', respuestas: ['FA Cup', 'Copa Libertadores', 'UEFA Champions League'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué jugador ha ganado más Balones de Oro?', respuestas: ['Cristiano Ronaldo', 'Lionel Messi', 'Michel Platini'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál es el estadio más grande del mundo por capacidad?', respuestas: ['Maracaná', 'Camp Nou', 'Rungrado 1st of May Stadium'], respuestaCorrecta: 2 },
+        { pregunta: '¿Qué equipo ganó la primera edición de la UEFA Champions League?', respuestas: ['Real Madrid', 'AC Milan', 'Benfica'], respuestaCorrecta: 0 },
+        { pregunta: '¿Qué país ha ganado más medallas de oro olímpicas en fútbol?', respuestas: ['Brasil', 'Hungría', 'Argentina'], respuestaCorrecta: 1 },
+        { pregunta: '¿Cuál es el récord de asistencia en un partido de fútbol femenino?', respuestas: ['90,185', '60,739', '101,254'], respuestaCorrecta: 2 },
+        { pregunta: '¿Qué equipo es conocido como "Los Invencibles" en el contexto de una temporada sin derrotas?', respuestas: ['Arsenal', 'Juventus', 'AC Milan'], respuestaCorrecta: 0 }
     ]
 };
-    
